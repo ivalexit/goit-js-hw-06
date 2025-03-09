75352ff1a8afe72185bea6f262ecc46cfd5304ad
ivalexit <ivalextar@gmail.com>
2025-03-09 18:10:11 +0100

    task-2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,27 @@
-function showThis() {
-    console.log(this);
+class Storage {
+    #items;
+  
+    constructor(items) {
+      this.#items = items;
+    }
+  
+    getItems() {
+      return this.#items;
+    }
+  
+    addItem(newItem) {
+      this.#items.push(newItem);
+    }
+  
+    removeItem(itemToRemove) {
+      this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
   }
   
-  const user = {
-    name: "Олексій",
-    showContext: showThis, // Присвоїли функцію
-  };
+  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+  storage.addItem("Droid");
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+  storage.removeItem("Prolonger");
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
   
-  user.showContext();
